7bca80f8ecb593a38ccce7dfc711935ac80b651b
Ali Kemal <95899041+alikemalcimsit@users.noreply.github.com>
2025-01-08 00:11:28 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,12 +5,12 @@ import { getFirestore, setDoc, doc } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDxht5FEtfRoqRFhING03lSo_cuCEexjpE",
-  authDomain: "evently-665a5.firebaseapp.com",
-  projectId: "evently-665a5",
-  storageBucket: "evently-665a5.firebasestorage.app",
-  messagingSenderId: "2408840794",
-  appId: "1:2408840794:web:bb021b04a066b63603e263"
+  apiKey: "API KEY",
+  authDomain: "DOMAIN",
+  projectId: "PROJECT-ID",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
